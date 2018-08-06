42f2a0abc72c5ae5e93cd8dbbd8efc23e4e8af0a
Phil DeMonaco <pdemon@gmail.com>
2018-08-06 11:19:04 -0400

    Block Mirrors
    We don't need to be waiting around 1000 years for the artifactory server
    to time out.

--- configs/components/rubygem-httpclient.rb
+++ configs/components/rubygem-httpclient.rb
@@ -4,7 +4,7 @@ component "rubygem-httpclient" do |pkg, settings, platform|
   pkg.version "2.8.3"
   pkg.md5sum "0d43c4680b56547b942caa0d9fefa8ec"
   pkg.url "https://rubygems.org/downloads/httpclient-#{pkg.get_version}.gem"
-  pkg.mirror "#{settings[:buildsources_url]}/httpclient-#{pkg.get_version}.gem"
+  #pkg.mirror "#{settings[:buildsources_url]}/httpclient-#{pkg.get_version}.gem"
 
   pkg.install do
     ["#{settings[:gem_install]} httpclient-#{pkg.get_version}.gem"]
